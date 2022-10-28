1436805592971458a45976cf5ac89c2b791e7cc7
mrmx <mrmx@MacBook-Pro-mrmx.local>
2022-10-29 00:57:42 +0400

    Add configparser

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from telegram_bot_driver.telegram_bot import Bot, TelegramBot
 from zkhbotapp.api.zkh_bot_api_model import BotApiModel, ZKHBotApiModelImpl
 
+import configparser
+
+configParser = configparser.ConfigParser()
+configParser.read_file(open(r'config.txt'))
+token = configParser.get('Telegram', 'token')
+
 bot_api_model: BotApiModel = ZKHBotApiModelImpl()
 
-bot: Bot = TelegramBot(token="205081013:AAG-ysgYtwuBeSLLctTKThrA-Weri0Vh_6E", bot_api_model=bot_api_model)
+bot: Bot = TelegramBot(token=token, bot_api_model=bot_api_model)
 bot.launch()
